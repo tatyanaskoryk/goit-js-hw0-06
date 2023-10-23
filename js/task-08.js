8a44af8df6e25b263ae9fabeb154b1ace0d0e75f
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-10-23 15:09:39 +0300

    addtask8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
-
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+    event.preventDefault();
+    const email = loginForm.elements.email.value;
+    const password = loginForm.elements.password.value;
+    
+    if (!email || !password) {
+        alert('Всі поля мають бути заповнені');
+    } else { 
+        const listenerForm = {
+            email,
+            password
+        };
+        console.log(listenerForm);
+        loginForm.reset();
+     }
+    }
