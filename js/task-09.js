08dcff6a3fee001c75ad6adf7e4a2f7ef23eb8c6
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-10-24 22:17:01 +0300

    addtask9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
+const btnColor = document.querySelector('.change-color');
+const textColor = document.querySelector('.color');
+ function handleClick() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  textColor.textContent = randomColor; 
+  
+}
+
+btnColor.addEventListener("click", handleClick);
+
+
