d76629a6566c22bf0797af2c2dfd564bf70a4df5
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-10-28 11:50:36 +0300

    ADD TASK6 COMMIT CHANGE2

--- js/task-06.js
+++ js/task-06.js
@@ -1,19 +1,3 @@
-// const textInput = document.getElementById('validation-input');
-
-// textInput.addEventListener("blur", () => {
-//   const expectedLength = parseInt(textInput.getAttribute('data-length'));
-//   const currentLength = textInput.value.length;
-
-//   if (currentLength === expectedLength) {
-//     textInput.classList.remove('invalid');
-//     textInput.classList.add('valid');
-//   } else {
-//     textInput.classList.remove('valid');
-//     textInput.classList.add('invalid');
-//   }
-// });
-
-
 const textInput = document.getElementById('validation-input');
 function setValidationClass(a, b) {
   if (b) {
