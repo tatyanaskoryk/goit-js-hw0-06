0ef6f343f4126c3fb57ee3d8c8fb61709a6ad94f
tatyanaskoryk <137694687+tatyanaskoryk@users.noreply.github.com>
2023-10-28 11:10:59 +0300

    add task 5 change

--- js/task-05.js
+++ js/task-05.js
@@ -1,7 +1,9 @@
  
+
 const nameInput = document.getElementById('name-input');
 const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input', () => {
-  nameOutput.textContent = nameInput.value || 'Anonymous';
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue || 'Anonymous';
 });
